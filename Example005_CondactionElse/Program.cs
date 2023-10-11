20b265c14f91e8ea292f3db13ccdc8ac1c859899
alexwincode <aleksejfeliferov@gmail.com>
2023-10-11 18:25:15 +0300

    add if & else

---
+++ Example005_CondactionElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя  - ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "meri")
+{
+    Console.WriteLine("Ура, Это МАША");
+
+}
+
+else 
+{
+    Console.Write("Привет,   ");
+    Console.WriteLine(username);
+}
